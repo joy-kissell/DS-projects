58f8fe01be419686452cda7fcefae3061e081a59
Joy Kissell <dzp6qm@virginia.edu>
2025-01-04 18:17:40 -0500

    updated authenticate_twitter function to use environment variables

--- Sentiment_Analysis/dt_analysis.py
+++ Sentiment_Analysis/dt_analysis.py
@@ -7,10 +7,17 @@ from nltk import download
 
 nltk.download('vader_lexicon')
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 #authenticating my twitter development account
-def authenticate_twitter(api_key, api_key_secret, access_token, access_token_secret):
-    authenticate = tweepy.OAuthHandler(api_key, api_key_secret)
-    authenticate.set_access_token(access_token,access_token_secret)
-    return tweepy.API(authenticate, wait_on_rate_limit=True)
+def authenticate_twitter():
+    api_key = os.getenv("API_KEY")
+    api_key_secret = os.getenv("API_SECRET")
+    api_access = os.getenv("ACCESS_TOKEN")
+    api_access_secret = os.getenv("ACCESS_SECRET")
+    authenticate = tweepy.OAuthHandler(api_key,api_key_secret)
+    authenticate.set_access_token(api_access,api_access_secret)
+    return tweepy.API(authenticate, wait_on_rate_limit = True)
 
-
+api = authenticate_twitter()
