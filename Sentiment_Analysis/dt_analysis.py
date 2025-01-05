f1643cf853818885c64fbbd399e9eeb09b560289
Joy Kissell <dzp6qm@virginia.edu>
2025-01-05 17:16:54 -0500

    defined time range and tweet query, made get_tweets function

--- Sentiment_Analysis/dt_analysis.py
+++ Sentiment_Analysis/dt_analysis.py
@@ -2,6 +2,7 @@ import tweepy
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
+import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk import download 
 
@@ -9,7 +10,10 @@ nltk.download('vader_lexicon')
 
 from dotenv import load_dotenv
 import os
+
+#loading in key to twitter
 load_dotenv()
+
 #authenticating my twitter development account
 def authenticate_twitter():
     api_key = os.getenv("API_KEY")
@@ -21,3 +25,22 @@ def authenticate_twitter():
     return tweepy.API(authenticate, wait_on_rate_limit = True)
 
 api = authenticate_twitter()
+
+#to restrict time range of tweets
+import datetime
+#want to cover all options
+query = "Donald Trump OR President Trump OR Trump"
+
+#start term of presidency
+range_1_start = "2017-01-20"
+range_1_end = "2021-01-20"
+
+#reelection
+range_2_start = "2021-01-21"
+range_2_end = "2024-11-05"
+
+#fetching tweets that fit the query and each time range
+def get_tweets(query, start, end):
+    tweets = tweepy.Cursor(api.search_tweets, q=query, lang="en", since=start, until=end).items(1000)
+    return [tweet for tweet in tweets if not is_news_tweet(tweet)]
+
